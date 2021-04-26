468dd39792a10157c197fd609b432c861331c19b
Grenda Carla <grendacarlaferreira@gmail.com>
2021-04-25 21:39:08 -0300

    configurando firebase - etapa 4

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'dart:core';
 import 'package:agenda/Pag1.dart';
+//import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+Future<void> main() async {
+  //await Firebase.initializeApp();
   runApp(MyApp());
 }
 
